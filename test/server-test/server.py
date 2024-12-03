c07860ef60c27bafecda7a7896706c2ee9324eec
Rory Poulter <rorypoulter2@gmail.com>
2024-12-03 12:11:26 +0000

    Add conversions between both currencies for 1 unit

--- test/server-test/server.py
+++ test/server-test/server.py
@@ -8,7 +8,7 @@ from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 
-def convert(start_curr: str, final_curr: str, amount: str) -> str | int:
+def convert(start_curr: str, final_curr: str, amount: str, dplaces: int = 3) -> str | int:
     """Converts between the two currencies.     
     Error codes:        
     1: Start and final currencies are the same
@@ -27,8 +27,8 @@ def convert(start_curr: str, final_curr: str, amount: str) -> str | int:
         data = json.load(f)
     amount = float(amount)
     result = amount * data["rates"][final_curr] / data["rates"][start_curr]
-    return str(result)
-
+    return str(round(result, dplaces))
+    
 
 @app.route("/", methods=["GET", "POST"])
 def form():
@@ -42,9 +42,13 @@ def form():
         final_curr = request.form['final_currency']
         amount = request.form['amount']
         result = convert(start_curr, final_curr, amount)
+        start_to_final = f"1 {start_curr} = {convert(start_curr, final_curr, '1')} {final_curr}"
+        final_to_start = f"1 {final_curr} = {convert(final_curr, start_curr, '1')} {start_curr}"
         if isinstance(result, int):
             return jsonify({'error': result})
-        return jsonify({'output' : result})
+        return jsonify({'output': result,
+                        'start_to_final': start_to_final,
+                        'final_to_start': final_to_start})
     return render_template('index.html')
 
 
